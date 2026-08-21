2b68a72668e987b31e153c809bea8178fc63fa1d
paulo henrique albuquerque oliveira junior <paulo11780872@aluno.mg.gov.br>
2026-08-21 14:11:43 -0300

    feat: adicionar arrays de candidatos e matriz de votos

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SistemaVotacao {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de votação iniciado.");
+        scanner.close();
+    }
 }
+static final int MAX_CANDIDATOS = 5;
+static final int TOTAL_TURMAS = 3;
+static final int MAX_VOTANTES_POR_TURMA = 10;
+
+static int[] numerosCandidatos = new int[MAX_CANDIDATOS];
+static String[] nomesCandidatos = new String[MAX_CANDIDATOS];
+static int[] votosCandidatos = new int[MAX_CANDIDATOS];
+
+static int[][] votosPorTurma = new int[TOTAL_TURMAS][MAX_VOTANTES_POR_TURMA];
+
+static int[] quantidadeVotosTurma = new int[TOTAL_TURMAS];
+static int quantidadeCandidatos = 0;
