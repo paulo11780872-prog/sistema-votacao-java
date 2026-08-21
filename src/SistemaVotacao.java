5fd900f6a7a95a235fad96096a94d55047a45988
paulo henrique albuquerque oliveira junior <paulo11780872@aluno.mg.gov.br>
2026-08-21 14:39:16 -0300

    feat: implementar menu principal com do-while

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -2,22 +2,70 @@ import java.util.Scanner;
 
 public class SistemaVotacao {
 
+    // Scanner para entrada de dados
     static Scanner scanner = new Scanner(System.in);
 
+    // Constantes do sistema
+    static final int MAX_CANDIDATOS = 5;
+    static final int TOTAL_TURMAS = 3;
+    static final int MAX_VOTANTES_POR_TURMA = 10;
+
+    // Dados dos candidatos
+    static int[] numerosCandidatos = new int[MAX_CANDIDATOS];
+    static String[] nomesCandidatos = new String[MAX_CANDIDATOS];
+    static int[] votosCandidatos = new int[MAX_CANDIDATOS];
+
+    // Matriz de votos por turma
+    static int[][] votosPorTurma =
+            new int[TOTAL_TURMAS][MAX_VOTANTES_POR_TURMA];
+
+    // Controle das turmas e candidatos
+    static int[] quantidadeVotosTurma = new int[TOTAL_TURMAS];
+    static int quantidadeCandidatos = 0;
+
     public static void main(String[] args) {
-        System.out.println("Sistema de votação iniciado.");
+
+        int opcao;
+
+        do {
+            System.out.println("\n===== SISTEMA DE VOTAÇÃO =====");
+            System.out.println("1 - Cadastrar candidatos");
+            System.out.println("2 - Iniciar votação");
+            System.out.println("3 - Exibir resultado");
+            System.out.println("4 - Exibir matriz de votos");
+            System.out.println("5 - Sair");
+
+            System.out.print("Opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+
+                case 1:
+                    System.out.println("Cadastro selecionado.");
+                    break;
+
+                case 2:
+                    System.out.println("Votação selecionada.");
+                    break;
+
+                case 3:
+                    System.out.println("Resultado selecionado.");
+                    break;
+
+                case 4:
+                    System.out.println("Matriz selecionada.");
+                    break;
+
+                case 5:
+                    System.out.println("Sistema encerrado.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+
+        } while (opcao != 5);
+
         scanner.close();
     }
 }
-static final int MAX_CANDIDATOS = 5;
-static final int TOTAL_TURMAS = 3;
-static final int MAX_VOTANTES_POR_TURMA = 10;
-
-static int[] numerosCandidatos = new int[MAX_CANDIDATOS];
-static String[] nomesCandidatos = new String[MAX_CANDIDATOS];
-static int[] votosCandidatos = new int[MAX_CANDIDATOS];
-
-static int[][] votosPorTurma = new int[TOTAL_TURMAS][MAX_VOTANTES_POR_TURMA];
-
-static int[] quantidadeVotosTurma = new int[TOTAL_TURMAS];
-static int quantidadeCandidatos = 0;
