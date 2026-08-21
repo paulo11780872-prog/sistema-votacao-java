8d663fc6cdb4c5d6fd251b5e7369a438809f1e3a
paulo henrique albuquerque oliveira junior <paulo11780872@aluno.mg.gov.br>
2026-08-21 13:49:07 -0300

    chore: iniciar estrutura do projeto de votação

---
+++ src/SistemaVotacao.java
@@ -0,0 +1,12 @@
+
+public class SistemaVotacao {
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de Votação");
+        System.out.println("Sistema iniciado com sucesso!");
+    }
+}
+
+
+
+    
